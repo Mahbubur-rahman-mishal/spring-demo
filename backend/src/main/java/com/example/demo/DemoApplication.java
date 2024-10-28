931603254b65ddd36b80135fedd39ed5a783e6b3
MRMISHAL\misha <mishal.rahman996@gmai.com>
2024-10-28 10:02:39 +0900

    Backend working, hot reload not working

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -2,19 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@RestController
+// @EnableJpaRepositories("com.example.demo.repository")
 public class DemoApplication {
-
-	@RequestMapping("/")
-	public String home() {
-		return "Hello Docker World";
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
